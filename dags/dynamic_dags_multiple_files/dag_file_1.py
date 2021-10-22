b8b27a165d2dc7475833fff292692480bbed29d3
Domonique Gordon <46009602+DomoniqueGordon@users.noreply.github.com>
2021-10-22 14:23:41 -0700

    added dynamic_dags_multiple_files

---
+++ dags/dynamic_dags_multiple_files/dag_file_1.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+default_args = {"start_date": datetime(2021, 1, 1)}
+
+dag = DAG(
+    "dag_file_1",
+    schedule_interval="@daily",
+    default_args=default_args,
+)
+
+with dag:
+    extract_data = DummyOperator(task_id="extract-data")
+    transform_data = DummyOperator(task_id="transform-data")
+    load_data = DummyOperator(task_id="load-data")
---
+++ dags/dynamic_dags_multiple_files/dag_file_2.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+
+default_args = {"start_date": datetime(2021, 1, 1)}
+
+dag = DAG(
+    "dag_file_2",
+    schedule_interval="@monthly",
+    default_args=default_args,
+)
+
+with dag:
+    extract_data = DummyOperator(task_id="extract-data")
+    transform_data = DummyOperator(task_id="transform-data")
+    load_data = DummyOperator(task_id="load-data")
